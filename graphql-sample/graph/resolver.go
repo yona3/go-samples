a52d0b609438c3a4524d3e93cc13feae425bb1cb
yona3 <kou.y.0905@icloud.com>
2022-03-24 16:09:25 +0900

    add graphql-sample

---
+++ graphql-sample/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/yona3/go-samples/graphql-sample/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
